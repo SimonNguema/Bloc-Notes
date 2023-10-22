5cdc1be58ee3fd00013d7965f9a6c17e31616502
Votre nom <Votre adresse e-mail>
2023-10-22 06:18:53 +0000

    first commit

---
+++ show.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class Showpage extends StatefulWidget {
+  const Showpage({super.key});
+
+  @override
+  State<Showpage> createState() => _ShowpageState();
+}
+
+class _ShowpageState extends State<Showpage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
